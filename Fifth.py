8c972d96c98cf1fd1edc4091bf35ae885c4bba01
Kolayn <socialnetworkspost@gmail.com>
2016-12-30 10:53:01 +0300

    First

---
+++ Fifth.py
@@ -0,0 +1,18 @@
+class MoneyBox:
+    def __init__(self, capacity):
+        self.max = capacity
+        self.sum = 0
+
+
+    def can_add(self, v):
+        if (v+self.sum) > self.max:
+            return False
+        else:
+            return True
+
+    def add(self, v):
+        if self.can_add(v)==True:
+            self.sum += v
+            return
+        else:
+            return
